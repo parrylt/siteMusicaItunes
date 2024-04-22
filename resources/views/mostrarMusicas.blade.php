4642f3bb5e11129b6ef3420884719757d0e66af0
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2024-04-22 19:39:26 -0300

    Update mostrarMusicas.blade.php

--- resources/views/mostrarMusicas.blade.php
+++ resources/views/mostrarMusicas.blade.php
@@ -3,33 +3,29 @@
 <section class="container m-5">
 
   
-  <table class="table">
-
-    <tbody>
-     
+<div class="container">
+  <div class="row">
     @foreach($registroMusicas as $registroMusicasLoop)
-
-      <tr>
-      <div class="d-flex justify-content-center">
-      <div class="card" style="width: 21rem;">
-  <img src="{{$registroMusicasLoop->image}}" class="card-img-top" alt="...">
-  <div class="card-body">
-  <h5 class="card-title text-center fw-bolder">{{$registroMusicasLoop->nome}}</h5>
-            </br>
-              <p class="card-text">Banda: &nbsp;{{$registroMusicasLoop->banda}}</p>
-              <p class="card-text">Genero: &nbsp;{{$registroMusicasLoop->genero}}</p>
-              <p class="card-text">Valor: &nbsp;{{$registroMusicasLoop->valor}}</p>
-        <audio controls>
-          <source src="{{$registroMusicasLoop->musica}}" type="audio/mp3">
-        </audio>
-        <a href="{{$registroMusicasLoop->musica}}" download class="btn btn-primary mt-3">Baixar Ilegalmente</a>
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="{{$registroMusicasLoop->image}}" class="card-img-top" alt="Capa do Álbum">
+          <div class="card-body">
+            <h5 class="card-title text-center fw-bolder">{{$registroMusicasLoop->nome}}</h5>
+            <p class="card-text">Banda: &nbsp;{{$registroMusicasLoop->banda}}</p>
+            <p class="card-text">Gênero: &nbsp;{{$registroMusicasLoop->genero}}</p>
+            <p class="card-text">Valor: &nbsp;{{$registroMusicasLoop->valor}}</p>
+            <audio controls>
+              <source src="{{$registroMusicasLoop->musica}}" type="audio/mp3">
+            </audio>
+            <a href="{{$registroMusicasLoop->musica}}" download class="btn btn-primary mt-3">Baixar Ilegalmente</a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-      </tr>
     @endforeach
-    </tbody>
-  </table>
+  </div>
+</div>
+
+
 </section>
 
 @endsection
